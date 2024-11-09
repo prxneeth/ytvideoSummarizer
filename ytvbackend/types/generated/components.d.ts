53cf5a2db401b0be19e2dee7505af4e7b566473b
prxneeth <praneethreddyg1818@gmail.com>
2024-11-09 22:56:17 +0530

    features sections code refactoring

--- ytvbackend/types/generated/components.d.ts
+++ ytvbackend/types/generated/components.d.ts
@@ -1,5 +1,19 @@
 import type { Schema, Struct } from '@strapi/strapi';
 
+export interface ComponentsFeature extends Struct.ComponentSchema {
+  collectionName: 'components_components_features';
+  info: {
+    displayName: 'Feature';
+  };
+  attributes: {
+    heading: Schema.Attribute.String;
+    icon: Schema.Attribute.Enumeration<
+      ['CLOCK_ICON', 'CHECK_ICON', 'CLOUD_ICON']
+    >;
+    subHeading: Schema.Attribute.Text;
+  };
+}
+
 export interface ComponentsLink extends Struct.ComponentSchema {
   collectionName: 'components_components_links';
   info: {
@@ -9,6 +23,18 @@ export interface ComponentsLink extends Struct.ComponentSchema {
     isExternal: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
     text: Schema.Attribute.String;
     url: Schema.Attribute.String;
+  };
+}
+
+export interface LayoutFeaturesSection extends Struct.ComponentSchema {
+  collectionName: 'components_layout_features_sections';
+  info: {
+    displayName: 'Features Section';
+  };
+  attributes: {
+    description: Schema.Attribute.Text;
+    feature: Schema.Attribute.Component<'components.feature', true>;
+    title: Schema.Attribute.String;
   };
 }
 
@@ -29,7 +55,9 @@ export interface LayoutHeroSection extends Struct.ComponentSchema {
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
+      'components.feature': ComponentsFeature;
       'components.link': ComponentsLink;
+      'layout.features-section': LayoutFeaturesSection;
       'layout.hero-section': LayoutHeroSection;
     }
   }
