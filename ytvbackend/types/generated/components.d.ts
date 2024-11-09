d8ab9e4b45922d9601eb12b2ce47f94a03c19d6e
prxneeth <praneethreddyg1818@gmail.com>
2024-11-09 21:45:58 +0530

    setup and homepage strapi

---
+++ ytvbackend/types/generated/components.d.ts
@@ -0,0 +1,36 @@
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface ComponentsLink extends Struct.ComponentSchema {
+  collectionName: 'components_components_links';
+  info: {
+    displayName: 'Link';
+  };
+  attributes: {
+    isExternal: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    text: Schema.Attribute.String;
+    url: Schema.Attribute.String;
+  };
+}
+
+export interface LayoutHeroSection extends Struct.ComponentSchema {
+  collectionName: 'components_layout_hero_sections';
+  info: {
+    description: '';
+    displayName: 'Hero Section';
+  };
+  attributes: {
+    heading: Schema.Attribute.String;
+    image: Schema.Attribute.Media<'images'>;
+    link: Schema.Attribute.Component<'components.link', false>;
+    subHeading: Schema.Attribute.Text;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'components.link': ComponentsLink;
+      'layout.hero-section': LayoutHeroSection;
+    }
+  }
+}
